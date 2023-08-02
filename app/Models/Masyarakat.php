94ed594392b1c4eaffb43a6ae44746c2201c4f8d
rezawp <rezaaawp@gmail.com>
2023-08-02 09:23:04 +0700

    update : add model

---
+++ app/Models/Masyarakat.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Masyarakat extends Model
+{
+    use HasFactory;
+
+    protected $table = 'masyarakat';
+    protected $primaryKey = 'nik';
+    protected $keyType = 'char';
+    public $incrementing = false;
+}
