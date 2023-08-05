e98f66612e8bd88274ee167b8e245c2396c753ab
rezawp <rezaaawp@gmail.com>
2023-08-05 08:24:51 +0700

    update : add error page & change auth page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +44,56 @@
             </x-primary-button>
         </div>
     </form>
+</x-guest-layout> --}}
+
+<x-guest-layout>
+    <div class="page page-center">
+        <div class="container-tight py-4">
+            <div class="text-center mb-4"><a href="." class="navbar-brand navbar-brand-autodark"><img
+                        src="https://projects.rezawp.com/public/assets/img/logo 1,0.png" height="100"></a></div>
+            <form class="card card-md" autocomplete="off">
+                <div class="card-body">
+                    <h2 class="card-title text-center mb-4">Login to your account</h2>
+                    <div class="mb-3"><label class="form-label">Email address</label><input id="email"
+                            name="email" type="email" class="form-control" placeholder="Enter email"
+                            autocomplete="off"></div>
+                    <div class="mb-2"><label class="form-label">Password<span class="form-label-description"><a
+                                    href="http://localhost:8000/forgot-password">I forgot password</a></span></label>
+                        <div class="input-group input-group-flat"><input id="password" name="password" type="password"
+                                class="form-control" placeholder="Password" autocomplete="off">
+                            <div class="invalid-feedback">Invalid feedback</div><span class="input-group-text"><a
+                                    href="#" class="link-secondary" title="Show password"
+                                    data-bs-toggle="tooltip"><svg xmlns="http://www.w3.org/2000/svg" class="icon"
+                                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                        stroke="currentColor" fill="none" stroke-linecap="round"
+                                        stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <circle cx="12" cy="12" r="2"></circle>
+                                        <path
+                                            d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7">
+                                        </path>
+                                    </svg></a></span>
+                        </div>
+                    </div>
+                    <div class="mb-2"><label class="form-check"><input type="checkbox" class="form-check-input"><span
+                                class="form-check-label">Remember me on this device</span></label></div>
+                    <div class="form-footer"><button type="submit" class="btn btn-primary w-100">Sign in</button></div>
+                </div>
+                <div class="hr-text">or</div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col"><a href="/auth/google" class="btn btn-google w-100"><svg
+                                    xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <path d="M17.788 5.108a9 9 0 1 0 3.212 6.892h-8"></path>
+                                </svg>Login with Google</a></div>
+                    </div>
+                </div>
+            </form>
+            <div class="text-center text-muted mt-3">Don't have account yet? <a tabindex="-1"
+                    href="http://localhost:8000/register">Sign up</a></div>
+        </div>
+    </div>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -49,4 +49,73 @@
             </x-primary-button>
         </div>
     </form>
+</x-guest-layout> --}}
+
+<x-guest-layout>
+    <div class="page page-center">
+        <div class="container-tight">
+            <div class="text-center mb-4"><a href="." class="navbar-brand navbar-brand-autodark"><img
+                        src="https://projects.rezawp.com/public/assets/img/logo 1,0.png" height="100"></a></div>
+            <form class="card card-md">
+                <div class="card-body">
+                    <h2 class="card-title text-center mb-4">Create new account</h2>
+                    <div class="mb-3"><label class="form-label">Name</label><input id="name" name="name"
+                            type="text" class="form-control" placeholder="Enter name" value=""></div>
+                    <div class="mb-3"><label class="form-label">Email address</label><input id="email"
+                            name="email" type="email" class="form-control" placeholder="Enter email" value="">
+                    </div>
+                    <div class="mb-3"><label class="form-label">Password</label>
+                        <div class="input-group input-group-flat"><input id="password" name="password" type="password"
+                                class="form-control" placeholder="Password" autocomplete="off" value=""><span
+                                class="input-group-text"><a href="#" class="link-secondary" title="Show password"
+                                    data-bs-toggle="tooltip"><svg xmlns="http://www.w3.org/2000/svg" class="icon"
+                                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                        stroke="currentColor" fill="none" stroke-linecap="round"
+                                        stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <circle cx="12" cy="12" r="2"></circle>
+                                        <path
+                                            d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7">
+                                        </path>
+                                    </svg></a></span></div>
+                    </div>
+                    <div class="mb-3"><label class="form-label">Confirmation Password</label>
+                        <div class="input-group input-group-flat"><input id="password_confirmation"
+                                name="password_confirmation" type="password" class="form-control" placeholder="Password"
+                                autocomplete="off" value=""><span class="input-group-text"><a href="#"
+                                    class="link-secondary" title="Show password" data-bs-toggle="tooltip"><svg
+                                        xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                        stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <circle cx="12" cy="12" r="2"></circle>
+                                        <path
+                                            d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7">
+                                        </path>
+                                    </svg></a></span></div>
+                    </div>
+                    <div class="mb-3"><label class="form-check"><input type="checkbox" class="form-check-input"><span
+                                class="form-check-label">Agree the <a tabindex="-1" href="syarat-dan-ketentuan">terms
+                                    and
+                                    policy</a> .</span></label></div>
+                    <div class="form-footer"><button type="submit" class="btn btn-primary w-100">Create new
+                            account</button></div>
+                    <div class="hr-text">or</div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col"><a href="/auth/google" class="btn btn-google w-100"><svg
+                                        xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
+                                        height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                        fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M17.788 5.108a9 9 0 1 0 3.212 6.892h-8"></path>
+                                    </svg>Register with Google</a></div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <div class="text-center text-muted mt-3">Already have account? <a tabindex="-1"
+                    href="http://localhost:8000/login">Sign in </a></div>
+        </div>
+    </div>
 </x-guest-layout>
