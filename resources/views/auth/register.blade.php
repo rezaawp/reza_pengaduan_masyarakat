661e0e47f2036277608aa5dab2e2b210d06d8e74
rezawp <rezaaawp@gmail.com>
2023-08-08 07:26:56 +0700

    update : menambahkan validasi language dan menampilkan validasi di halaman register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -68,18 +68,22 @@
                         <label class="form-label">{{ __('Name') }}</label><input id="name" name="name"
                             type="text" class="form-control" placeholder="{{ __('Enter name') }}"
                             value="{{ old('name') }}" />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
                     </div>
                     <div class="mb-3">
                         <label class="form-label">{{ __('Email') }}</label><input id="email" name="email"
                             type="email" class="form-control" placeholder="{{ __('Enter email') }}"
                             value="{{ old('email') }}" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+
                     </div>
                     <div class="mb-3">
                         <label class="form-label">{{ __('Password') }}</label>
                         <div class="input-group input-group-flat">
                             <input id="password" name="password" type="password" class="form-control"
-                                placeholder="Password" autocomplete="off" value="" /><span
-                                class="input-group-text"><a href="#" class="link-secondary" title="Show password"
+                                placeholder="Password" autocomplete="off" value="" />
+                            <span class="input-group-text">
+                                <a href="#" class="link-secondary" title="Show password"
                                     data-bs-toggle="tooltip"><svg xmlns="http://www.w3.org/2000/svg" class="icon"
                                         width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
                                         stroke="currentColor" fill="none" stroke-linecap="round"
@@ -90,18 +94,20 @@
                                             d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7">
                                         </path>
                                     </svg></a></span>
+
                         </div>
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
                     </div>
                     <div class="mb-3">
                         <label class="form-label">{{ __('Confirm Password') }}</label>
                         <div class="input-group input-group-flat">
                             <input id="password_confirmation" name="password_confirmation" type="password"
-                                class="form-control" placeholder="Password" autocomplete="off" value="" /><span
-                                class="input-group-text"><a href="#" class="link-secondary" title="Show password"
-                                    data-bs-toggle="tooltip"><svg xmlns="http://www.w3.org/2000/svg" class="icon"
-                                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                                        stroke="currentColor" fill="none" stroke-linecap="round"
-                                        stroke-linejoin="round">
+                                class="form-control" placeholder="Password" autocomplete="off" value="" />
+                            <span class="input-group-text"><a href="#" class="link-secondary"
+                                    title="Show password" data-bs-toggle="tooltip"><svg
+                                        xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                        stroke-linecap="round" stroke-linejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                         <circle cx="12" cy="12" r="2"></circle>
                                         <path
@@ -109,6 +115,8 @@
                                         </path>
                                     </svg></a></span>
                         </div>
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+
                     </div>
                     <div class="mb-3">
                         <label class="form-check"><input type="checkbox" class="form-check-input" /><span
