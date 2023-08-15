a5ae495a58d7f0aee6ed2f3a888df8c0c43ce74c
rezawp <rezaaawp@gmail.com>
2023-08-15 13:00:53 +0700

    update : install volt

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\VoltServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
     ])->toArray(),
 
